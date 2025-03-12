6daaff4985e6890365451d04fb7212aced7a214c
uon22 <kmk0013@naver.com>
2025-03-12 15:40:56 +0900

    Commit in Visual Studio

--- 1완성_최지수_5645775_0311.cpp
+++ 1완성_최지수_5645775_0311.cpp
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 void main() {
-    int i = 31, num;
+    int i = 31, num; //초기값 31
 
-    printf("정수를 입력하세요: ");
-    scanf_s("%d", &num);
+    printf("정수를 입력하세요: "); //문장 출력
+    scanf_s("%d", &num); //정수를 입력받음
 
-    printf("입력된 정수의 32비트 표현: ");
+    printf("입력된 정수의 32비트 표현: "); //문장 출력
 
-    for (i; i >= 0; --i) {
-        printf("%d", (num >> i) & 1);
+    for (i; i >= 0; --i) { //초기값 31부터 0까지 1씩 감소하며 반복
+        printf("%d", (num >> i) & 1); //num을 i만큼 오른쪽으로 이동, & 1로 최하위 비트값 추출
     }
 
-    printf("\n");
+    printf("\n"); //줄바꿈
 }
