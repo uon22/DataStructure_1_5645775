45f69256696c63d4010b1032678e18e602828e00
uon22 <144295654+uon22@users.noreply.github.com>
2025-04-12 20:12:10 +0900

    Add files via upload

---
+++ linearList_14완성_최지수_5645775_0410.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "linearList.h"
+
+listType* createList(int size) {
+    listType* lptr = (listType*)malloc(sizeof(listType));
+    lptr->array = (elementType*)malloc(sizeof(elementType) * size);
+    lptr->size = size;
+    lptr->last = -1;
+    lptr->move = 0;
+    return lptr;
+}
+
+int destroyList(listType* list) {
+    free(list->array);
+    free(list);
+    return 1;
+}
+
+int initList(listType* list) {
+    list->last = -1;
+    list->move = 0;
+    return 1;
+}
+
+elementType readItem(listType* list, int index) {
+    if (index < 0 || index > list->last) {
+        fprintf(stderr, "Index error %d in readItem\n", index);
+        return NULL_ITEM;
+    }
+    return list->array[index];
+}
+
+elementType deleteItem(listType* list, int index) {
+    if (index < 0 || index > list->last) {
+        fprintf(stderr, "Index error %d in deleteItem\n", index);
+        return NULL_ITEM;
+    }
+    elementType removed = list->array[index];
+    for (int i = index; i < list->last; i++) {
+        list->array[i] = list->array[i + 1];
+        list->move++;
+    }
+    list->last--;
+    return removed;
+}
+
+int insertItem(listType* list, int index, elementType item) {
+    if (index < 0 || index > list->last + 1) return -1;
+    for (int i = list->last + 1; i > index; i--) {
+        list->array[i] = list->array[i - 1];
+        list->move++;
+    }
+    list->array[index] = item;
+    list->last++;
+    return 1;
+}
+
+int ordered_insertItem(listType* list, elementType item) {
+    int i, j;
+    for (i = 0; i <= list->last; i++) {
+        if (list->array[i].expo < item.expo) break;
+    }
+    for (j = list->last + 1; j > i; j--) {
+        list->array[j] = list->array[j - 1];
+        list->move++;
+    }
+    list->array[i] = item;
+    list->last++;
+    return 1;
+}
+
+int printList(listType* list) {
+    printf("List: size = %d, last = %d, move = %d\n", list->size, list->last, list->move);
+    printf("    Items: P(x) = ");
+    for (int i = 0; i <= list->last; i++) {
+        elementType t = list->array[i];
+        if (t.coef == 0) continue;
+
+        if (i > 0 && t.coef > 0) printf("+");
+        if (t.expo == 0) printf("%d", t.coef);
+        else if (t.expo == 1) printf("%dx", t.coef);
+        else printf("%dx^%d", t.coef, t.expo);
+    }
+    printf("\n");
+    return 1;
+}
---
+++ main.c
@@ -0,0 +1,81 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "linearList.h"
+
+#define MAX_INPUT 256
+
+void parsePolynomialInput(char* input, listType* poly) {
+    int coef = 0, expo = 0;
+    char* token = strtok(input, "+");
+
+    while (token != NULL) {
+        if (strchr(token, 'x') || strchr(token, 'X')) {
+            if (strchr(token, '^')) {
+                sscanf_s(token, "%dx^%d", &coef, &expo);
+            }
+            else {
+                sscanf_s(token, "%dx", &coef);
+                expo = 1;
+            }
+        }
+        else {
+            coef = atoi(token);
+            expo = 0;
+        }
+        ordered_insertItem(poly, (elementType) { coef, expo });
+        token = strtok(NULL, "+");
+    }
+}
+
+listType* multiplyPoly(listType* a, listType* b) {
+    listType* result = createList(a->size * b->size);
+    for (int i = 0; i <= a->last; i++) {
+        for (int j = 0; j <= b->last; j++) {
+            elementType t1 = a->array[i];
+            elementType t2 = b->array[j];
+            elementType temp = { t1.coef * t2.coef, t1.expo + t2.expo };
+
+            int found = 0;
+            for (int k = 0; k <= result->last; k++) {
+                if (result->array[k].expo == temp.expo) {
+                    result->array[k].coef += temp.coef;
+                    found = 1;
+                    break;
+                }
+            }
+            if (!found) ordered_insertItem(result, temp);
+        }
+    }
+    return result;
+}
+
+int main() {
+    char input1[MAX_INPUT], input2[MAX_INPUT];
+
+    printf("첫 번째 다항식을 입력하세요 (예: 4x^3+3x^2+3):\n");
+    fgets(input1, MAX_INPUT, stdin);
+    input1[strcspn(input1, "\n")] = 0;
+
+    printf("두 번째 다항식을 입력하세요 (예: 3x+7):\n");
+    fgets(input2, MAX_INPUT, stdin);
+    input2[strcspn(input2, "\n")] = 0;
+
+    listType* poly1 = createList(50);
+    listType* poly2 = createList(50);
+
+    parsePolynomialInput(input1, poly1);
+    parsePolynomialInput(input2, poly2);
+
+    listType* result = multiplyPoly(poly1, poly2);
+
+    printf("\n[곱셈 결과]\n");
+    printList(result);
+
+    destroyList(poly1);
+    destroyList(poly2);
+    destroyList(result);
+    return 0;
+}
